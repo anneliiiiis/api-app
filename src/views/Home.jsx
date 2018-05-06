4db08f1ccff55c9bbe532ad27e1c629bed9defd9
Anneliis Halling <anneliis.halling@wisemedia.ee>
2018-05-06 11:29:12 +0300

    better home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,21 +3,26 @@ import { Button } from 'react-bootstrap';
 
 import { Link } from 'react-router-dom';
 
+const ShowButton = props => {
+  const link = props.link;
+  const name = props.name;
+  const buttonClass = props.buttonClass;
+  return(
+     <Link to={link}>
+          <Button className= {buttonClass} >
+            { name }
+          </Button>
+        </Link>
+  )
+}
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container">
-        <Link to={`/users`}>
-          <Button className="btn-info" >
-            Show users
-          </Button>
-        </Link>
-        <Link to={`/posts`}>
-          <Button className= "btn-info" >
-            Show posts
-          </Button>
-        </Link>
+        <h1>HOME</h1>
+        <ShowButton link={`/users`} buttonClass="btn-info" name="Show users"/>
+        <ShowButton link={`/posts`} buttonClass="btn-info" name="Show posts"/>
       </div>
     );
   }
